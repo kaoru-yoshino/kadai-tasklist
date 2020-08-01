43823c891ee2353511c53d30e17df0c5d5a1bde2
kaoru.yoshino <test.yoshino.kaoru@gmail.com>
2020-08-01 01:30:34 +0000

    kadai add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,8 +47,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+            "status" => "required|max:10",
+            "content" => "required",
+            ]);
+        
         //postでtasks/にアクセスされた場合の「新規登録処理」
         $task = new Task;
+        $task->status = $request->status;   //statusの追加
         $task->content = $request->content;
         $task->save();
 
@@ -98,10 +105,17 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        //バリデーション
+        $request->validate([
+            "status" => "required|max:10",
+            "content" => "required",
+            ]);
+        
         //putまたはpatchでtasks/(任意のid)にアクセスされた場合の「更新処理」
          // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
